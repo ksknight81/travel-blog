e58860dca3587923d6dc43dec233531603e17d4a
kevin knight <ksknight81@gmail.com>
2022-08-11 19:24:54 -0400

    added additional files

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,8 +3,7 @@ INSERT INTO review
 
 VALUES 
 ('Toronto', 'Canada', 'November 26, 2021', '4.5', 'The trip was amazing! Filled with lots of places to visit'),
-('Vancouver, 'Canada', 'March 15, 2022', '4', 'Rainy city but great food'),
-('Tokyo', 'Japan', 'October 18, 2020', '4.7', 'A fast paced city with a lot of people')
-
-
+('Vancouver', 'Canada', 'March 15, 2022', '4', 'Rainy city but great food'),
+('Tokyo', 'Japan', 'October 18, 2020', '4.7', 'A fast paced city with a lot of people'),
+('New York', 'USA', 'September 10, 2022', '4', 'A large city filled with a great transit system to get around places')
 ;
