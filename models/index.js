1dc08520c07d14b09ebe619adf5487b0b647ec6a
Omar Rodriguez <omar.rguez26@gmail.com>
2022-08-11 20:31:50 -0400

    saved merged

--- models/index.js
+++ models/index.js
@@ -1,13 +1,4 @@
-<<<<<<< HEAD
 const User = require('./User');
 const Post = require('./Post');
 
 module.exports = { User, Post };
-=======
-const Review = require('./Review');
-const Comment = require('./Comment');
-
-
-
-module.exports = {Review, Comment};
->>>>>>> feature/comments
