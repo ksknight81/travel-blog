cce972e910b266d8f1098855f964579a1ac9b4a0
kevin knight <ksknight81@gmail.com>
2022-08-11 20:18:29 -0400

    added info to server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,10 +7,24 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Hello World'
-    });
+// connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost', 
+        user: 'root',
+        password: 'Che$terC@t123', 
+        database: 'travel_blog'
+    },
+    console.log('Connected to the travel blog database')
+);
+
+')
+db.query(`SELECT * FROM review`, (err, rows) => {
+    console.log(rows);
+});
+
+app.use((req, res) => {
+    res.status(404).end();
 });
 
 app.listen(PORT, () => {
