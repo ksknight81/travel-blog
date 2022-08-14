f48c05b0b98d8c4e7c28ef8816786eaf8a0efa16
Omar Rodriguez <omar.rguez26@gmail.com>
2022-08-14 13:16:12 -0400

    set up comment routes

--- routes/apiRoutes/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-// add routes definitions here
-const commentRoutes = require('./commentRoutes');
-
-// call out routers here
-router.use('/comments', commentRoutes);
-
-module.exports = router;
