6282ef66c2ec8bc259f9522bf2d4eb0a11f7f169
kevin knight <ksknight81@gmail.com>
2022-08-16 18:40:04 -0400

    added heroku port

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ const sess = {
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
