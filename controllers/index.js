d5db5e67ba2a530dbdee4a4239dadaf15a5c2b3e
Jaryd Knight <jaryd.knight@hotmail.com>
2022-08-15 23:10:47 -0400

    all handlebars templates working. comments partial needs to be tested

--- controllers/index.js
+++ controllers/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
+const signupRoutes = require('./signup-routes.js');
 
 router.use('/api', apiRoutes);
 router.use('/home', homeRoutes);
+router.use('/signup', signupRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
