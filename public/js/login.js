158a8944301f25faeb9aebbf7a14497bf7565025
Omar Rodriguez <omar.rguez26@gmail.com>
2022-08-16 23:19:05 -0400

    added login js functionality

--- public/js/login.js
+++ public/js/login.js
@@ -1,17 +1,13 @@
-async function signupFormHandler (event) {
+async function loginHandler (event) {
     event.preventDefault();
-    //console.log("submitting");
-
-    const username = document.querySelector("input[id='username'").value.trim();
     const email = document.querySelector("input[id='email'").value.trim();
     const password = document.querySelector("input[id='password'").value.trim();
 
-    if (username && email && password) {
-        const response = await fetch('http://localhost:3001/api/users', {
+    if (email && password) {
+        const response = await fetch('http://localhost:3001/api/users/login', {
             method: 'post',
             // mode: 'no-cors',
             body: JSON.stringify({
-                username,
                 email,
                 password
             }),
@@ -20,15 +16,16 @@ async function signupFormHandler (event) {
         });
 
         if (response.ok) {
-            //console.log('success');
+            fetch('http://localhost:3001/home');
             document.location.replace('/home');
+            // response.redirected('back');
+            // .then(response.render('homepage'));
+            
         } else {
             alert(response.statusText);
         }
     }
-
 }
 
-
-document.getElementById("signup-form")
-  .addEventListener("submit", signupFormHandler);
+document.getElementById("login-form")
+  .addEventListener("submit", loginHandler);
